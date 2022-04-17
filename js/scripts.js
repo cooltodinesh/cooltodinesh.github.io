95ee6006d6de3dabf3bf8472fd3e21b7862ba3e9
kapilbhosale <kapilbhosale@hotmail.com>
2022-04-17 13:13:14 +0530

    initial site

---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+/*!
+* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    
+    // Navbar shrink function
+    var navbarShrink = function () {
+
+
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            // logo_img.src = "assets/adcast_logo_white.png";
+            document.getElementById("logo_img_white").style.display = "block";
+            document.getElementById("logo_img_black").setAttribute("style", "display: none !important");
+            navbarCollapsible.classList.remove('navbar-shrink');            
+        } else {
+            // logo_img.src = "assets/adcast_logo.png";
+            document.getElementById("logo_img_black").style.display = "block";
+            document.getElementById("logo_img_white").setAttribute("style", "display: none !important");
+            navbarCollapsible.classList.add('navbar-shrink');            
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
